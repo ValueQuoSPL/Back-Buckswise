9c300be0954d2a44bba3309465c89d9b90abb288
sheetal <sheetal.s@valuequo.com>
2018-10-30 16:32:09 +0530

    Success Page URL for Prod

--- src/main/java/com/valuequo/buckswise/web/rest/SuccessPageResource.java
+++ src/main/java/com/valuequo/buckswise/web/rest/SuccessPageResource.java
@@ -52,7 +52,7 @@ public class SuccessPageResource {
 			@RequestParam String txnid, @RequestParam String productinfo, @RequestParam String email,
 			@RequestParam String amount, @RequestParam String addedon) {
 		successAndFailService.saveTransaction(mihpayid, status, txnid, productinfo, email, amount, addedon);
-		return new RedirectView("http://localhost:9000/#/success");
+		return new RedirectView("https://wwww.buckswise.com/#/success");
 	}
 	
 }
