b897cb381f26bc33a24f2b93b9474109812a38ae
sandeeppote <38154598+sandeeppote@users.noreply.github.com>
2019-01-23 12:41:06 +0530

    updates

--- src/main/java/com/valuequo/buckswise/service/AmfiService.java
+++ src/main/java/com/valuequo/buckswise/service/AmfiService.java
@@ -23,9 +23,6 @@ public class AmfiService {
     @Autowired
     private AmcRepository amcRepository;
 
-    @Autowired
-    private AmcRepository amcRepository;
-
     @Transactional
 	public void save(ArrayList<AmfiDTO> al) {
         int size = al.size();
