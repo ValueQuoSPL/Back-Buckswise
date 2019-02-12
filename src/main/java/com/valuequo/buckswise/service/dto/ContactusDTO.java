ce7f99e25c890e62fef4f3a8122aea11b9e0f03a
pratik-bali <pratik.bali@valuequo.com>
2019-02-12 19:28:26 +0530

    Refer a friend mail sending

--- src/main/java/com/valuequo/buckswise/service/dto/ContactusDTO.java
+++ src/main/java/com/valuequo/buckswise/service/dto/ContactusDTO.java
@@ -18,8 +18,8 @@ public class ContactusDTO implements Serializable {
     private String phone;
 
     private String msg;
+    private String yourname; // added by ranjan
 
-    private String yourname; 
 
     public Long getId() {
         return id;
@@ -61,13 +61,15 @@ public class ContactusDTO implements Serializable {
         this.msg = msg;
     }
 
-    public String getYourname() {
+    // added by ranjan --start
+     public String getYourname() {
         return yourname;
     }
 
     public void setYourname(String yourname) {
         this.yourname = yourname;
     }
+    // end
 
     @Override
     public boolean equals(Object o) {
@@ -98,7 +100,7 @@ public class ContactusDTO implements Serializable {
             ", email='" + getEmail() + "'" +
             ", phone=" + getPhone() +
             ", msg='" + getMsg() + "'" +
-            ", yourname='" + getYourname() + "'" +
+            ", yourname='" + getYourname() + "'" + // added by ranjan
             "}";
     }
 }
