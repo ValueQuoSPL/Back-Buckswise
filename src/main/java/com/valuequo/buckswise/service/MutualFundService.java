b93750fd81a0995503e5a504681250e4d650ceab
sandeeppote <sandeep.pote@valuequo.com>
2019-07-31 22:51:27 +0530

    indexoutofbound_issue_nav

--- src/main/java/com/valuequo/buckswise/service/MutualFundService.java
+++ src/main/java/com/valuequo/buckswise/service/MutualFundService.java
@@ -57,7 +57,7 @@ public class MutualFundService {
 
     String[] days = new String[] { "day1", "day2", "day3", "day4", "day5", "day6", "day7", "day8", "day9", "day10",
             "day11", "day12", "day13", "day14", "day15", "day16", "day17", "day18", "day19", "day20", "day21", "day21",
-            "day22", "day23", "day24", "day25", "day26", "day27", "day28", "day29", "day30", "day31", };
+            "day22", "day23", "day24", "day25", "day26", "day27", "day28", "day29", "day30", "day31"};
 
     public MutualFundService(MutualFundRepository mutualfundRepository, MutualFundMapper mutualfundMapper,
             AmfiRepository amfiRepository, EntityManagerFactory factory) {
@@ -163,6 +163,7 @@ public class MutualFundService {
             x.add(var.getDay29());
             x.add(var.getDay30());
             x.add(var.getDay31());
+            x.add(null);
         }
 
         for (int i = 0; i < days.length; i++) {
